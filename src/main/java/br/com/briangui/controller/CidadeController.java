877f4469efb09de46cec46d9196f6c89c052acea
Rodrigo Gonçalves  - BR Consórcios - Sistemas <rodrigo.goncalves@consorciouniaoldb.com.br>
2022-12-23 14:17:09 -0300

    add - telas contato, cidade e usuario

---
+++ src/main/java/br/com/briangui/controller/CidadeController.java
@@ -0,0 +1,58 @@
+package br.com.briangui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CidadeController {
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Button btnNovo;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TextField tfCep;
+
+    @FXML
+    private TextField tfCidade;
+
+    @FXML
+    private TextField tfId;
+
+    @FXML
+    private TextField tfPesquisa;
+
+    @FXML
+    private TextField tfUf;
+
+    @FXML
+    void excluirRegistro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void incluirRegistro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarRegistro(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/br/com/briangui/controller/ContatoController.java
@@ -0,0 +1,80 @@
+package br.com.briangui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ContatoController {
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Button btnNovo;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private ComboBox<?> cbCidade;
+
+    @FXML
+    private ComboBox<?> cbTipoContato;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TextField tfCep;
+
+    @FXML
+    private TextField tfDataNascimento;
+
+    @FXML
+    private TextField tfEndereco;
+
+    @FXML
+    private TextField tfId;
+
+    @FXML
+    private TextField tfNomeContato;
+
+    @FXML
+    private TextField tfNumero;
+
+    @FXML
+    private TextField tfPesquisa;
+
+    @FXML
+    private TextField tfTelefone1;
+
+    @FXML
+    private TextField tfTelefone2;
+
+    @FXML
+    private TextField tfUf;
+
+    @FXML
+    void excluirRegistro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void incluirRegistro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarRegistro(ActionEvent event) {
+
+    }
+
+}
